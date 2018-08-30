03c18686a18fb34b5ffd4f21ee9e581df5045a0b
alexandor3x <alejandroiglesias@gmail.com>
2018-08-30 17:43:53 -0300

    resolviendo problemas de token null

--- push-notification.js
+++ push-notification.js
@@ -3,9 +3,16 @@ import firebase from 'firebase';
 export const initializeFirebase = () => {
  
 
- 
+  var config = {
+    apiKey: "AIzaSyACaL6qsfIAgQ1QUCWhPmyn76D1UL54VmA",
+    authDomain: "push-test-de5c2.firebaseapp.com",
+    databaseURL: "https://push-test-de5c2.firebaseio.com",
+    projectId: "push-test-de5c2",
+    storageBucket: "push-test-de5c2.appspot.com",
+    messagingSenderId: "824440442769"
+  };
 
-  firebase.initializeApp({messagingSenderId: "824440442769"});
+  firebase.initializeApp(config);
 
 
 }
@@ -21,6 +28,7 @@ export const  askForPermissioToReceiveNotifications = () => {
    })
    
    return prom1.then((token)=>{
+    console.log(token);
     fetch("https://iid.googleapis.com/iid/v1/"+ token + "/rel/topics/all",{
       method: 'POST',
       headers:{'Authorization':'key=AAAAv_R6z5E:APA91bHLXWzm5oMfs44Q6iZRxOQhI9-kQKaxpeY497zYE_XamXGlPymImjGhXpst_zjv89h4zrRWh7DmoPpw8rCnHXhmHAIcSWl75A6d5sPq0TNgLDGaemewxe7BIhW4fqLt5nLFekDX',
